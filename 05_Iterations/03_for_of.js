0f3ffe5a0a589e76fe4bc43547076042bf92d84f
Nikhil-Arjun <nikhilarjun97@gmail.com>
2023-08-04 10:50:32 +0530

    for_of

--- 05_Iterations/03_for_of.js
+++ 05_Iterations/03_for_of.js
@@ -1,8 +1,12 @@
 // for of
+// -> The for...of statement creates a loop iterating over iterable objects (including Array, Map, set, arguments Objects and so on), invoking a custom iteration hook with statements to be executed for the value of each distinct property.
 
 // ["","",""]
 // [{},{},{}]
 
+// syntax ->
+// for (variable of object)
+//   statement
 const arr = [1, 2, 3, 4, 5];
 
 for (const num of arr) {
@@ -14,6 +18,9 @@ const greetings = 'Hello World';
 for (const greet of greetings) {
   // console.log(`Each char is ${greet}`);
 }
+
+//Map->
+//The Map object holds key-value pairs and remembers the original insertion order of the keys. Any value (both objects and primitive values) may be used as either a key or a value.
 
 const map = new Map();
 map.set('IN', 'India');
