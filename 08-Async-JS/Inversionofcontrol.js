e1925d84739e88bd936ac70306a3cf2836d2101d
Nikhil-Arjun <nikhilarjun97@gmail.com>
2023-08-16 21:34:59 +0530

    Inversion of control

--- 08-Async-JS/Inversionofcontrol.js
+++ 08-Async-JS/Inversionofcontrol.js
@@ -1,3 +1,10 @@
+//Disadvantages of Callbacks
+
+// 1. Callback hell
+
+// 2. Invversion of control ->
+// If there is some part of code which control we are passing to someone else, & we don't lnow how that part eill be executed, this problem called as inversion of control
+
 function fun(inputString, fn) {
   // execute some algorithms the string
   let output = inputString.split(','); //Output is an array
