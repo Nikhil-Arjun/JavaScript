b496e45349bd5a6aeda486206611e4d60a771b3d
Nikhil-Arjun <nikhilarjun97@gmail.com>
2023-08-20 15:55:39 +0530

    demo-2

---
+++ 10-Promise/demo2.js
@@ -0,0 +1,20 @@
+console.log("Strat of the file.");
+
+setTimeout(function timer1() {
+  console.log("Timer 1 done.");
+  let y = Promise.resolve("Immediately promise");
+  y.then(function promiseY(value) {
+    console.log("Whose Promise ?", value);
+  });
+}, 0);
+
+let x = Promise.resolve("Nikhil's promise");
+x.then(function processPromise(value) {
+  console.log("Whose promise ?", value);
+});
+
+setTimeout(function timer2() {
+  console.log("Timer 2 done.");
+}, 0);
+
+console.log("End of the file.");
